d110367276c381acb64afb7463fb80188e7cad18
ClebersonGarcia05 <cleberson.garcia051299@gmail.com>
2022-06-21 15:39:02 -0300

    Exercicio 89 finalizado

--- Mundo python 3/exercicios/ex089.py
+++ Mundo python 3/exercicios/ex089.py
@@ -4,7 +4,7 @@ de cada aluno individualmente. """
 
 lista = [[], [], [], []]
 while True:
-    lista[0].append(str(input('Digite o nome do aluno: ')))
+    lista[0].append(str(input('Digite o nome do aluno: ')).capitalize().strip())
     lista[1].append(float(input('Digite a primeira nota: ')))
     lista[2].append(float(input('Digite a segunda nota: ')))
 
@@ -13,10 +13,14 @@ while True:
         r = str(input('Opção inválida, deseja continuar cadastrando? [S/N]')).upper().strip()[0]
     if r == 'N':
         break
+print(f'\n{"NOME":<12} {"MÉDIA":^12}')
 for pos, v in enumerate(lista[0]):
-    for i in lista[1]:
-        print(i)
-        for j in lista[2]:
-            print(j)
-            lista[3].append((i + j) / 2)
-    print(f'O aluno {v} teve a média {lista[3][pos]}')
+    lista[3].append((lista[1][pos] + lista[2][pos]) / 2)
+    print(f'{v:<12} {lista[3][pos]:^12}')
+nome = str(input('\nDeseja visualizar as notas de qual aluno? [0 para sair]: ')).capitalize().strip()
+if nome == 0:
+    exit()
+print(f'{"NOME":<12} {"NOTA 1":^12} {"NOTA 2":^12}')
+for pos, v in enumerate(lista[0]):
+    if v == nome:
+        print(f'{v:<12} {lista[1][pos]:^12} {lista[2][pos]:^12}')
