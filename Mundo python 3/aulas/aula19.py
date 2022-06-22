a211dc705ff589b2b056f37aa1a5e3cd47efbd8a
ClebersonGarcia05 <cleberson.garcia051299@gmail.com>
2022-06-22 16:54:21 -0300

    Adionado novos desafios

---
+++ Mundo python 3/aulas/aula19.py
@@ -0,0 +1,16 @@
+""" DICIONÀRIOS
+
+filme = {} Cria um dicionário chamado filme
+
+filme['Nome'] = Star Wars Cria um indice 'Nome' e adicionado 'Star wars' como valor
+
+del filme['Nome'] Apaga o indice e o valor do dicionário
+
+print(filme.values()) Mostra todos os valores dentro de cada indice do dicionário
+
+print(filme.keys()) Mostra todos os indices dentro do dicionário
+
+print(filme.items()) Mostra os valores e os indices
+
+filme.copy() Cria uma cópia do dicionário
+ """
