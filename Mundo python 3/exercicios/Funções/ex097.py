cebc53bed5c91478a85d2b92019a79acf4694bfc
ClebersonGarcia05 <cleberson.garcia051299@gmail.com>
2022-06-27 17:12:52 -0300

    Exercícios 096 a 100

---
+++ Mundo python 3/exercicios/Funções/ex097.py
@@ -0,0 +1,10 @@
+""" Faça um programa que tenha uma função chamada escreva() , que receba um texto qualquer como parâmetro e mostre uma 
+mensagem com tamanho adptável.
+
+EX:
+escreva('Olá mundo!')
+
+Saída:
+----------
+Olá mundo!
+----------"""
