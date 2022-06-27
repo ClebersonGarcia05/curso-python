ef00410168857fcb848ac7a5b70f72e868567b7b
ClebersonGarcia05 <cleberson.garcia051299@gmail.com>
2022-06-27 13:58:44 -0300

    Exercicio 95 finalizado

--- Mundo python 3/exercicios/Dicionários/ex095.py
+++ Mundo python 3/exercicios/Dicionários/ex095.py
@@ -2,16 +2,17 @@
 aproveitamento de cada jogador. """
 
 jog = {}
-jogadores = []
+time = []
 gols = []
 while True:
     jog['nome'] = str(input('Digite o nome do jogador: '))
     part = int(input(f'Digite quantas partidas o {jog["nome"]} jogou: '))
     for i in range(part):
-        gols.append(int(input(f'Quantidade de gols feitos na {i+1}º partida: ')))
+        gols.append(int(input(f'  --  Quantidade de gols feitos na {i+1}º partida: ')))
     jog['gols'] = gols.copy()
     jog['total'] = sum(gols)
-    jogadores.append(jog.copy())
+    time.append(jog.copy())
+    gols.clear()
     while True:
         r = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
         if r in 'SN':
@@ -21,13 +22,25 @@ while True:
             print()
     if r == 'N':
         break
-print('-=' * 30)
-print(jogadores)
-print('-=' * 30)
-for k, v in jog.items():
-    print(f'O campo {k} tem o valor {v}')
-print('-=' * 30)
-print(f'O jogador {jog["nome"]} jogou {part} partidas.')
-for i, v in enumerate(gols):
-    print(f'=> Na partida {i+1}, fez {v} gols.')
-print(f'Foi um total de {jog["total"]} gols')
+print(f'{"Nº":^4} {"Nome":<15} {"gols":<15} {"Total":<15}')
+for i, j in enumerate(time):
+    print(f'{i:^4} ', end='')
+    for k, v in j.items():
+        print(f'{str(v):<15} ', end='')
+    print()
+while True:
+    part = 0
+    n = int(input('Mostrar dados de qual jogador? (999 para parar): '))
+    if n == 999:
+        break
+    if n != i:
+        print('\033[31mCódigo errado, tente novamente!\033[m')
+    for i, v in enumerate(time):
+        if n == i:
+            print(f'Dados do jogador {v["nome"]}: ')
+            for k, nv in v.items():
+                if k == 'gols':
+                    for l in nv:
+                        print(f'  --  No jogo {part+1} fez {l} gols')
+                        part += 1  
+print('Programa finalizado!')
