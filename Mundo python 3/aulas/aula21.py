c6f25add728d225f33591d486fb746321958e70c
ClebersonGarcia05 <cleberson.garcia051299@gmail.com>
2022-06-29 17:25:13 -0300

    Aula 21 e exercícios 101 a 106 adicionados

---
+++ Mundo python 3/aulas/aula21.py
@@ -0,0 +1,10 @@
+""" Interactive help = ajuda interativa, para chamar é só codar help() 
+
+docstrings = É um comentário de bloco dentro da função que descreve para que a função serve e para que cada parametro 
+serve
+
+Parametros opcionais =  def somar (a=0, b=0, c=0) = colocar o parametro recebendo 0 deixa eles como parametros opcionais.
+
+
+"""
+print(input.__doc__)
